8e5039d8babdc998f9978efae615aace2c8f79cc
Ankit <ankity1892003@gmail.com>
2024-04-07 01:14:10 +0530

    email verification added

--- src/routes/user_validation_routes.js
+++ src/routes/user_validation_routes.js
@@ -6,12 +6,15 @@ const user_auth = require("../../middleware/user_auth.js");
 const {
   user_login,
   user_register,
-  user_logout
+  user_logout,
+  email_verify
 } = require("../controllers/user_controller.js");
 
 router.post("/login", user_login);
 
 router.post("/register", user_register);
+
+router.post("/email_verify",email_verify);
 
 router.post("/logout",user_auth,user_logout)
 
